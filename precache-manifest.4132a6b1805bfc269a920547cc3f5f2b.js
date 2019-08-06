5284c408c2c610eb3dfafd093bc00250bb7560bc
Mauro Jr <maurocsjr@gmail.com>
2019-08-06 16:45:58 -0400

    Updates

---
+++ precache-manifest.4132a6b1805bfc269a920547cc3f5f2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6958397fc68adb9c8a096e7bee1959d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1e2690e4d85cf1df346b",
+    "url": "/monsters-rolodex/static/css/main.96857b01.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "1e2690e4d85cf1df346b",
+    "url": "/monsters-rolodex/static/js/main.903da7e9.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
